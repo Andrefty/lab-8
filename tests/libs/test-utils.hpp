44d8df8f92c0d0a37a64fc48a2ecdce0165c56c3
cmarius46 <cmarius46@gmail.com>
2021-04-06 16:37:07 +0300

    added tests

--- tests/libs/test-utils.hpp
+++ tests/libs/test-utils.hpp
@@ -29,6 +29,9 @@
 #define TASK3_TEST_FILENAME1 "../data/test3-data1.txt"
 #define TASK3_TEST_FILENAME2 "../data/test3-data2.txt"
 
+#define TASK3_TEST_FILENAME1 "../data/test3-data3.txt"
+#define TASK3_TEST_FILENAME2 "../data/test3-data4.txt"
+
 graph_list* readList(const char * ); 
 graph_matrix* readMatrix(const char * );
 std::vector<std::vector<int>> readRoads(const char * );
