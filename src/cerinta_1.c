71e2bfe51b962a719748ac837084caf48c347eef
Andrei Farcasanu <tudor.farcasanu@stud.acs.upb.ro>
2021-04-21 18:40:47 +0300

    matrix to list done?

--- src/cerinta_1.c
+++ src/cerinta_1.c
@@ -1,6 +1,5 @@
 #include "utils.h"
 
-
 /**
  * TODO:   O functie ce transforma un graf reprezentat de o matrice de adiacenta 
  * intr-unul reprezentat cu ajutorul listelor de adiacenta
@@ -8,8 +7,35 @@
  * @param  graph: Graful in reprezentarea matrice de adiacenta
  * @retval - Graful in reprezentarea liste de adiacenta
  */
-graph_list* matrixToList(graph_matrix* graph) {
-
+graph_list *matrixToList(graph_matrix *graph)
+{
+    graph_list *g = (graph_list *)malloc(sizeof(graph_list));
+    g->V=0;
+    g->E=0;
+    if (g == NULL)
+    {
+        printf(" no memory for this graph");
+        return NULL;
+    }
+    g->listHeads = (node **)calloc(graph->V, sizeof(node *));
+    for (int i = 0; i < graph->V; i++)
+    {
+        node *newnode = (node *)malloc(sizeof(node));
+        newnode->vertexNr = i;
+        newnode->next = NULL;
+        g->listHeads[i] = newnode;
+        node *p = g->listHeads[i];
+        for (int j = 0; j < graph->V; j++)
+            if (graph->matrix[i][j] == 1)
+            {
+                node *newnode = (node *)malloc(sizeof(node));
+                newnode->vertexNr = j;
+                newnode->next = NULL;
+                p->next = newnode; //adaug varful
+                p = p->next;
+                (g->E)++;
+            }
+    }
 }
 
 /**
@@ -18,19 +44,16 @@ graph_list* matrixToList(graph_matrix* graph) {
  * @param  graph: Graful in reprezentarea matrice de adiacenta, a carui memorie trebuie eliberata
  * @retval 
 */
-void free_graph_matrix(graph_matrix** graph) {
-    
+void free_graph_matrix(graph_matrix **graph)
+{
 }
 
-
-node* push_values(node* root, int val) {
+node *push_values(node *root, int val)
+{
     // nice to have; functie de adauga o valoare intr-o lista
 }
 
-
-void print_graph_list(graph_list* graph) {
+void print_graph_list(graph_list *graph)
+{
     // nice to have; functie care afiseaza continutul unui graf reprezentat prin liste de adiacenta
 }
-
-
-
