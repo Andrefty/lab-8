e8e32dccc3375a1af6097993a63c88f53a77f8dc
tudor.farcasanu <tudor.farcasanu@fep7-1.grid.pub.ro>
2021-04-24 00:34:18 +0300

    Ready

--- src/cerinta_3.c
+++ src/cerinta_3.c
@@ -9,13 +9,10 @@
  */
 int **findPaths(graph_matrix *graph)
 {
-    int **ret = (int **)calloc(graph->V, sizeof(int *));
-    for (int i = 0; i < graph->V; i++)
-        *ret = (int *)calloc(graph->V, sizeof(int));
     for (int k = 0; k < graph->V; k++)
         for (int i = 0; i < graph->V; i++)
             for (int j = 0; j < graph->V; j++)
-                if (graph->matrix[i][j] == 0)
-                    ret[i][j] = graph->matrix[i][k] * graph->matrix[k][j];
-    return ret;
+                if (graph->matrix[i][j] == 0&&i!=j)
+                    graph->matrix[i][j] = graph->matrix[i][k] * graph->matrix[k][j];
+    return graph->matrix;
 }
