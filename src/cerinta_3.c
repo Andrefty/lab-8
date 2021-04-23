f805aa2beca007b32f81be692fbd10bbc84d597d
tudor.farcasanu <tudor.farcasanu@fep7-1.grid.pub.ro>
2021-04-23 23:39:04 +0300

    cerinta 3 sigsegv

--- src/cerinta_3.c
+++ src/cerinta_3.c
@@ -7,6 +7,15 @@
  * @param  graph: Graful in reprezentarea matrice de adiacenta
  * @retval - o matrice reprezentand matricea drumurilor
  */
-int** findPaths(graph_matrix* graph) {
-    
+int **findPaths(graph_matrix *graph)
+{
+    int **ret = (int **)calloc(graph->V, sizeof(int *));
+    for (int i = 0; i < graph->V; i++)
+        *ret = (int *)calloc(graph->V, sizeof(int));
+    for (int k = 0; k < graph->V; k++)
+        for (int i = 0; i < graph->V; i++)
+            for (int j = 0; j < graph->V; j++)
+                if (graph->matrix[i][j] == 0)
+                    ret[i][j] = graph->matrix[i][k] * graph->matrix[k][j];
+    return ret;
 }
